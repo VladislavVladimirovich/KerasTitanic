e159e1b2a78939a538def21c5e9665fbe695612d
Vladislav <vdiss08@mail.ru>
2022-06-24 02:38:00 +0300

    ok

--- Example.py
+++ Example.py
@@ -8,7 +8,7 @@ output_data = np.array([0.5, 0.9, 1.0])
 model = k.Sequential()
 model.add(k.layers.Dense(units=1, activation="linear"))
 model.compile(loss="mse", optimizer="sgd")
-fit_results = model.fit(x=tf.expand_dims(input_data, axis=-1), y=output_data, epochs=100)
+fit_results = model.fit(x=tf.expand_dims(input_data, axis=1), y=output_data, epochs=100)
 
 predicted = model.predict([0.5])
 print(predicted)
